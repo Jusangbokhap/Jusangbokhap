4833edad81d26508576fa098a3f67bb4d82c05cf
JinjinBread <pge0720@naver.com>
2025-02-06 12:42:20 +0900

    initialize

---
+++ src/main/java/jsbh/Jusangbokhap/JusangbokhapApplication.java
@@ -0,0 +1,13 @@
+package jsbh.Jusangbokhap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusangbokhapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JusangbokhapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jsbh/Jusangbokhap/JusangbokhapApplicationTests.java
@@ -0,0 +1,13 @@
+package jsbh.Jusangbokhap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusangbokhapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
