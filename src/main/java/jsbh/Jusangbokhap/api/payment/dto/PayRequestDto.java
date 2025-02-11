3484d11203ab034baaebc6622509456b01ae0598
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-11 22:51:47 +0900

    feat: PayRequestDto 필수 필드 및 유효성 검사

--- src/main/java/jsbh/Jusangbokhap/api/payment/dto/PayRequestDto.java
+++ src/main/java/jsbh/Jusangbokhap/api/payment/dto/PayRequestDto.java
@@ -1,6 +1,8 @@
 package jsbh.Jusangbokhap.api.payment.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -8,18 +10,24 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class PayRequestDto {
-    @Schema(description = "주문번호")
+
+    @Schema(description = "예약번호")
+    @NotBlank(message = "예약번호는 필수 입력 값입니다.")
     private String orderId;
 
     @Schema(description = "사용자 ID")
+    @NotBlank(message = "사용자 ID는 필수 입력 값입니다.")
     private String userId;
 
     @Schema(description = "숙소 정보")
+    @NotBlank(message = "숙소 정보는 필수 입력 값입니다.")
     private String itemName;
 
     @Schema(description = "총 결제 금액")
+    @Positive(message = "결제 금액은 0보다 커야 합니다.")
     private int totalAmount;
 
     @Schema(description = "결제할 숙소 수량")
+    @Positive(message = "숙소 수량은 0보다 커야 합니다.")
     private int quantity;
 }
