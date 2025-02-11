ab6af2191cfc457114cffffc3f15c3e8dad1e0ba
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-11 22:51:59 +0900

    feat: RedisTemplate 설정 및 트랜잭션 지원 비활성화

--- src/main/java/jsbh/Jusangbokhap/common/config/RedisConfig.java
+++ src/main/java/jsbh/Jusangbokhap/common/config/RedisConfig.java
@@ -3,7 +3,6 @@ package jsbh.Jusangbokhap.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -11,16 +10,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setEnableTransactionSupport(false);
         return redisTemplate;
     }
 }
