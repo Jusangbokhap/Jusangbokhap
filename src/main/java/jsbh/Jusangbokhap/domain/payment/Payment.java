ce43bf563f4cfe4e1a4aecc7b0d55bb5da0a1caf
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-11 13:42:26 +0900

    feat: 결제 엔티티 및 저장 기능

--- src/main/java/jsbh/Jusangbokhap/domain/payment/Payment.java
+++ src/main/java/jsbh/Jusangbokhap/domain/payment/Payment.java
@@ -1,26 +1,17 @@
 package jsbh.Jusangbokhap.domain.payment;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
+import lombok.*;
 
-import jsbh.Jusangbokhap.domain.BaseEntity;
-import jsbh.Jusangbokhap.domain.reservation.Reservation;
 import jsbh.Jusangbokhap.domain.user.User;
-
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jsbh.Jusangbokhap.domain.reservation.Reservation;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Payment extends BaseEntity {
+@AllArgsConstructor
+@Builder
+public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,13 +21,11 @@ public class Payment extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reservation_id", nullable = false)
     private Reservation reservation;
 
     private Integer price;
-
     private String paymentMethod;
-
     private String paymentStatus;
 }
