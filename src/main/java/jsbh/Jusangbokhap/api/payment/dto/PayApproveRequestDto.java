237f69f43fdb0ce44a6048f90b14fd3f5abf1b9c
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-11 22:48:04 +0900

    feat: PayApproveRequestDto 필드 유효성 검사

--- src/main/java/jsbh/Jusangbokhap/api/payment/dto/PayApproveRequestDto.java
+++ src/main/java/jsbh/Jusangbokhap/api/payment/dto/PayApproveRequestDto.java
@@ -1,6 +1,7 @@
 package jsbh.Jusangbokhap.api.payment.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -8,15 +9,20 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class PayApproveRequestDto {
-    @Schema(description = "주문번호")
+
+    @Schema(description = "예약번호")
+    @NotBlank(message = "예약번호는 필수 입력 값입니다.")
     private String orderId;
 
     @Schema(description = "사용자 ID")
+    @NotBlank(message = "사용자 ID는 필수 입력 값입니다.")
     private String userId;
 
     @Schema(description = "카카오페이에서 반환한 결제 승인 토큰")
+    @NotBlank(message = "결제 승인 토큰(pgToken)은 필수 입력 값입니다.")
     private String pgToken;
 
     @Schema(description = "결제 고유 번호")
+    @NotBlank(message = "결제 고유 번호(TID)는 필수 입력 값입니다.")
     private String tid;
 }
