7d33bca56dbef8354782b314ab1d22e00dc28dfe
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-11 14:00:09 +0900

    feat: TID 기반 중복 결제 확인 및 예약 ID로 결제 내역 조회 기능

--- src/main/java/jsbh/Jusangbokhap/domain/payment/PaymentRepository.java
+++ src/main/java/jsbh/Jusangbokhap/domain/payment/PaymentRepository.java
@@ -3,6 +3,10 @@ package jsbh.Jusangbokhap.domain.payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByReservation_ReservationId(Long reservationId);
+    boolean existsByTid(String tid);
 }
