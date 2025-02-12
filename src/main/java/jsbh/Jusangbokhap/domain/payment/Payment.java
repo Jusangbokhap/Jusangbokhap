35b2391cce73d1062a1576c533d006d5dd7545da
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-12 11:30:55 +0900

    feat: Payment 엔티티 결제 상태 업데이트 및 검증 로직 추가

--- src/main/java/jsbh/Jusangbokhap/domain/payment/Payment.java
+++ src/main/java/jsbh/Jusangbokhap/domain/payment/Payment.java
@@ -1,14 +1,15 @@
 package jsbh.Jusangbokhap.domain.payment;
 
 import jakarta.persistence.*;
-import lombok.*;
-
-import jsbh.Jusangbokhap.domain.user.User;
+import jsbh.Jusangbokhap.api.payment.dto.PayApproveResponseDto;
+import jsbh.Jusangbokhap.common.exception.CustomException;
+import jsbh.Jusangbokhap.common.exception.ErrorCode;
 import jsbh.Jusangbokhap.domain.reservation.Reservation;
+import jsbh.Jusangbokhap.domain.user.User;
+import lombok.*;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
@@ -26,7 +27,10 @@ public class Payment {
     @JoinColumn(name = "reservation_id", nullable = false)
     private Reservation reservation;
 
+    @Column(nullable = false)
     private Integer price;
+
+    @Column(nullable = false)
     private String paymentMethod;
 
     @Enumerated(EnumType.STRING)
@@ -35,4 +39,24 @@ public class Payment {
 
     @Column(nullable = false, unique = true)
     private String tid;
+
+    public void updatePaymentOnSuccess(PayApproveResponseDto responseDto) {
+        this.paymentStatus = PaymentStatus.COMPLETED;
+        this.price = responseDto.getAmount().getTotal();
+        this.paymentMethod = responseDto.getPayment_method_type();
+    }
+
+    public void updatePaymentOnFailure() {
+        this.paymentStatus = PaymentStatus.FAILED;
+    }
+
+    public void validateBeforeCancel() {
+        if (this.paymentStatus == PaymentStatus.CANCELED) {
+            throw new CustomException(ErrorCode.PAYMENT_ALREADY_CANCELED);
+        }
+    }
+
+    public void cancel() {
+        this.paymentStatus = PaymentStatus.CANCELED;
+    }
 }
