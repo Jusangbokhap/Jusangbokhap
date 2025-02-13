9c7eb1d21ed9fe2966878e71366f621b4f8e1cd3
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-13 23:18:13 +0900

    refactor: ErrorCode 클래스 에러 메시지 수정 및 새로운 에러 추가

--- src/main/java/jsbh/Jusangbokhap/common/exception/ErrorCode.java
+++ src/main/java/jsbh/Jusangbokhap/common/exception/ErrorCode.java
@@ -17,14 +17,14 @@ public enum ErrorCode {
 
     // 결제 관련 오류
     PAYMENT_REQUEST_FAILED(HttpStatus.BAD_REQUEST, "카카오페이 결제 요청 중 오류가 발생했습니다. 다시 시도해 주세요."),
-    INVALID_PG_TOKEN(HttpStatus.BAD_REQUEST, "결제 승인 토큰(pgToken)이 유효하지 않습니다."),
     KAKAO_PAY_INVALID_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "카카오페이 API 응답이 올바르지 않습니다."),
-    REDIS_FETCH_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "카카오페이 결제 정보를 찾을 수 없습니다."),
-    PAYMENT_ALREADY_PROCESSED(HttpStatus.BAD_REQUEST, "이미 결제가 완료된 주문입니다."),
     NOT_FOUND_PAYMENT(HttpStatus.NOT_FOUND, "해당 예약의 결제 내역이 없습니다."),
     KAKAO_PAY_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "카카오페이 API 호출 중 오류가 발생했습니다."),
     PAYMENT_ALREADY_CANCELED(HttpStatus.BAD_REQUEST, "이미 취소된 결제입니다."),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INVALID_AMOUNT(HttpStatus.BAD_REQUEST, "유효하지 않은 금액입니다."),
+    TID_NOT_FOUND(HttpStatus.NOT_FOUND, "결제 정보를 찾을 수 없습니다. TID 또는 주문번호를 확인하세요.");
+
 
     private final HttpStatus status;
     private final String message;
