db3de8aa96b45afc3bce0728a61a36fc6677481a
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-02-13 23:37:22 +0900

    feat: User 엔티티에 Lombok @Builder 어노테이션 추가

--- src/main/java/jsbh/Jusangbokhap/domain/user/User.java
+++ src/main/java/jsbh/Jusangbokhap/domain/user/User.java
@@ -1,21 +1,15 @@
 package jsbh.Jusangbokhap.domain.user;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jsbh.Jusangbokhap.domain.BaseEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "users")
+@Builder
+@AllArgsConstructor
 public class User extends BaseEntity {
 
     @Id
@@ -30,12 +24,4 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private String nickname;
-
-    @Builder
-    public User(String email, String password, String nickname) {
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-    }
-
 }
