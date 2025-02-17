16457a0fd9768d1618da2f12db9b37fa13a27f50
dltjdnjs <148112372+SeoWonLeee@users.noreply.github.com>
2025-02-17 23:48:57 +0900

    Update AccommodationPrice.java

--- src/main/java/jsbh/Jusangbokhap/domain/accommodation/AccommodationPrice.java
+++ src/main/java/jsbh/Jusangbokhap/domain/accommodation/AccommodationPrice.java
@@ -1,5 +1,6 @@
 package jsbh.Jusangbokhap.domain.accommodation;
 
+
 import jakarta.persistence.Embeddable;
 import jsbh.Jusangbokhap.api.accommodation.exception.AccommodationCustomException;
 import jsbh.Jusangbokhap.api.accommodation.exception.AccommodationErrorCode;
@@ -16,17 +17,17 @@ public class AccommodationPrice {
 
     private Integer price;
 
-    public AccommodationPrice(Integer price) {
-        validateAccommodationPrice(price);
-        this.price = price;
-    }
-
     public static AccommodationPrice from(Integer price) {
         return new AccommodationPrice(price);
     }
 
     public void updatePrice(Integer price) {
         if (price == null) return;
+        validateAccommodationPrice(price);
+        this.price = price;
+    }
+
+    private AccommodationPrice(Integer price) {
         validateAccommodationPrice(price);
         this.price = price;
     }
