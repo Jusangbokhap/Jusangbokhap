72ec086c6fc2ff92df4c9cf62f590cd0c3ba2e1d
jinhosoon <jinho7329@gmail.com>
2025-02-17 21:08:22 +0900

    fix: AccommodationHostController, AccommodationHostController 엔드포인트 겹치는 부분 수정

--- src/main/java/jsbh/Jusangbokhap/api/accommodation/controller/AccommodationHostController.java
+++ src/main/java/jsbh/Jusangbokhap/api/accommodation/controller/AccommodationHostController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/accommodations")
+@RequestMapping("/api/host/accommodations")
 @RequiredArgsConstructor
 public class AccommodationHostController {
 
