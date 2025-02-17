c46cc8325bdae38218919c2c683d2d4ea23ea0f5
dltjdnjs <148112372+SeoWonLeee@users.noreply.github.com>
2025-02-17 23:44:32 +0900

    Update ObjectMapperConfig.java

--- src/main/java/jsbh/Jusangbokhap/common/config/ObjectMapperConfig.java
+++ src/main/java/jsbh/Jusangbokhap/common/config/ObjectMapperConfig.java
@@ -1,25 +1,20 @@
 package jsbh.Jusangbokhap.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
-@EnableRedisRepositories
-public class RedisConfig {
+public class ObjectMapperConfig {
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-        redisTemplate.setEnableTransactionSupport(true);
-        return redisTemplate;
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+        return objectMapper;
     }
 }
