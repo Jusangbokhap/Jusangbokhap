d07bbee971f8e9602cc0321ec71333da9f5adc96
JinjinBread <pge0720@naver.com>
2025-02-18 23:26:20 +0900

    feat: update search(by coordinate) request & response DTO

--- src/main/java/jsbh/Jusangbokhap/api/accommodation/dto/AccommodationCoordSearchResponse.java
+++ src/main/java/jsbh/Jusangbokhap/api/accommodation/dto/AccommodationCoordSearchResponse.java
@@ -1,7 +1,38 @@
 package jsbh.Jusangbokhap.api.accommodation.dto;
 
+import jsbh.Jusangbokhap.domain.accommodation.Accommodation;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class AccommodationCoordSearchResponse {
 
+    private String businessName;
+    private String title;
+    private String address;
+    private Integer price;
+    private String accommodationType;
 
+    @Builder
+    private AccommodationCoordSearchResponse(String businessName, String title, String address,
+                                             Integer price, String accommodationType) {
+        this.businessName = businessName;
+        this.title = title;
+        this.address = address;
+        this.price = price;
+        this.accommodationType = accommodationType;
+    }
+
+    public static AccommodationCoordSearchResponse of(Accommodation accommodation) {
+        return AccommodationCoordSearchResponse.builder()
+                .businessName(accommodation.getBusinessName())
+                .title(accommodation.getTitle())
+                .address(accommodation.getAddress().getFullAddress())
+                .price(accommodation.getAccommodationPrice().getPrice())
+                .accommodationType(accommodation.getAccommodationType().name())
+                .build();
+    }
 
 }
